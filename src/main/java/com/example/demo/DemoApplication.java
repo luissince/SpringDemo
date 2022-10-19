8f8744b8238fb06fcfcd71f8038be6f38280e486
Ruber Flores <ruberfc@gmail.com>
2022-10-19 16:47:39 -0500

    Primer commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping(value="/")
+	public String getMethodName() {
+		return DemoApplication.class.getName();
+	}
+
+}
